a87341d7d62463c94b3de7108e5034c056495751
DuarteVonFaul <duarteriograndense@gmail.com>
2022-10-17 12:20:51 -0300

    FEFO Class ainda a revisar RemoveEntity()

--- FIFO, LIFO, FEFO/CPP/FEFO.cpp
+++ FIFO, LIFO, FEFO/CPP/FEFO.cpp
@@ -1,4 +1,206 @@
 #include <iostream>
-#include "Node.h"
+
 
 using namespace std;
+
+//Diferente do FIFO e LIFO o FEFO precisa de parametros mais complexos para definir quem vai sair
+//Esses parametros eu coloquei como Prioridade e Posição
+//Quem tiver a maior prioridade e a menor posição sai primeiro
+
+class Entity{
+	private:
+		int priority;
+		int pos;
+		Entity* next;
+		Entity* back;
+		int element;
+	
+	public:
+		
+		Entity(int p, int e, int pos){
+			this->priority = p;
+			this->pos	   = pos;
+			this->setBack(NULL);
+			this->setNext(NULL);
+			this->setElement(e);
+		}
+		
+		//getters
+			int getPos(){
+				return pos;
+			}
+			
+			int getPriority(){
+				return priority;
+			}
+			Entity* getBack()
+			{
+				return back;
+			}
+			Entity* getNext()
+			{
+				return next;
+			}
+			
+			int getElement()
+			{
+				return element;
+			}
+		//setters
+			int setPos(int p){
+				pos = p;
+			}
+			
+			int setPriority(int p){
+				priority = p;
+			}
+			void setBack(Entity* n)
+			{
+				back = n;
+			}
+			void setNext(Entity* n)
+			{
+				next = n;
+			}
+			
+			void setElement(int e)
+			{
+				element = e;
+			}	
+};
+
+
+const int MAX_Entity = 10;
+
+class FEFO{
+	private:
+		Entity* first;
+		Entity* end;
+		int len;
+	public:
+	//Constructor
+	
+		FEFO(){
+			first = NULL;
+			end	  = NULL;
+			len   = 0;
+		}
+		FEFO(int e, int p){
+			first = new Entity(p,e,1);
+			end = first;
+			len = 1;	
+		}
+		
+	//FEFO Method's
+		void addEntity(int e, int p){//Adicionar Entity do FEFO
+			if(!full()){
+				Entity* n = new Entity(p, e, len + 1);
+				n->setBack(end);
+				end->setNext(n);
+				end=n;
+				len++;
+			}else{
+				cout << "FIFO está Cheio" << endl;
+			}	
+		}
+		
+		void removeEntity(){//Remover Entity do FEFO	
+			if(!empty()){
+				Entity* aux = end;
+				for(int i=0; i<len; i++){
+					if(aux->getPriority() < aux->getBack()->getPriority()){	
+						aux = aux->getBack();
+					}
+					else if(aux->getPriority() == aux->getBack()->getPriority()){
+						if(aux->getPos() > aux->getBack()->getPos()){
+							aux = aux->getBack();
+						}
+					}
+				}
+				if(aux->getNext() == NULL){
+					aux->getBack()->setNext(NULL);
+					end = aux->getBack();
+					
+					
+				}else if(aux->getBack() == NULL){
+					aux->getNext()->setBack(NULL);
+					first = aux->getNext();
+					
+				}else{
+					aux->getBack()->setNext(aux->getNext());
+					aux->getNext()->setBack(aux->getBack());
+				}
+				
+				delete aux;
+				len--;
+				
+			}
+		}
+		
+		bool empty(){//Retorna se o FEFO é Vazio
+			return (len == 0);
+		}
+		
+		
+		bool full(){//Retorna se o FEFO está cheio
+			return (len == MAX_Entity);
+		}
+		
+		void printALL(){
+			printALL(first);
+		}
+		
+		void printALL(Entity* n){
+			if(n == NULL){
+				return;
+			}else{
+				cout << " Elemento: " << n->getElement() 
+					 << " Prioridade " << n->getPriority() 
+					 << " Posição: " << n->getPos()  
+					 << endl;
+				printALL(n->getNext());
+			}
+		}
+		
+	//setters
+		void setFirst(Entity* n){
+			first = n;
+		}
+		void setEnd(Entity* n){
+			end = n;
+		}
+		void setLen(int i){
+			len = i;
+	}
+	//getters
+		Entity* getFirst(){
+			return first;
+		}
+		Entity* getEnd(){
+			return end;
+		}
+		int	  getLen(){
+			return len;
+		}
+};
+
+
+int main(){
+	FEFO f = FEFO(1,3);
+	f.addEntity(2,3);
+	f.addEntity(3,4);
+	f.printALL();
+	cout << "-----------------" << endl;
+	f.removeEntity();
+	f.printALL();
+	cout << "-----------------" << endl;
+	f.removeEntity();
+	f.printALL();
+	cout << "-----------------" << endl;
+	
+	
+	
+	return 1;
+}
+
+
